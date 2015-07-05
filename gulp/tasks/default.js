5cbf0bb2fcc3b0a258d93d3c662fc67b4e2fd335
Ethan Leon <ethan@thoughtbot.com>
2015-07-05 17:28:43 -0500

    Add example image

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 
 gulp.task('watch', function() {
   // Watch .scss files
-  gulp.watch('_sass/**/*.scss', ['css']);
+  gulp.watch('_sass/**/*.scss', ['css', 'jekyll-rebuild']);
   // Watch .js files
-  gulp.watch('_js/**/*.js', ['js']);
+  gulp.watch('_js/**/*.js', ['js', 'jekyll-rebuild']);
   // Watch .html files and posts
   gulp.watch(['*.html', '_includes/**/*.html', '_layouts/*.html', '*.md', '_posts/*', '_data/**/*'], ['jekyll-rebuild']);
 });
