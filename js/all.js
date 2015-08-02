5de0cf50497ea54e1aa45773b40ecdcfdf245a72
Ethan Leon <ethan@thoughtbot.com>
2015-08-02 15:49:42 -0500

    Remove curtain and animations

--- js/all.js
+++ js/all.js
@@ -23102,7 +23102,7 @@ $('.curtain').each(function(){
 var articleId = 100;
 
 $('article').each(function(){
-  $(this).attr('id', 'article'+articleId).css('z-index', articleId--);
+  $(this).attr('id', 'article'+articleId);//.css('z-index', articleId--);
 });
 
 var bgId = 0;
@@ -23157,75 +23157,75 @@ $('img[alt*=" "]').each(function(){
   $(this).parent().append(caption);
 });
 
-(function($) {
-
-  function Curtain($curtains) {
-    var self = this,
-        $el = $curtains,
-        waypoints = [],
-        Waypoint = window.Waypoint;
-
-    self.init = function() {
-      self.setWaypoints();
-    }
-
-    self.setTop = function($el){
-      $('.curtain').removeClass('top');
-      $el.addClass('top');
-    }
-
-    self.injectFixed = function($el){
-      if ($el.length) {
-        $('#fixed').empty();
-        $el.children().first().clone().appendTo('#fixed');
-      }
-    }
-
-    self.setWaypoints = function(){
-      $('.curtain').each(function(){
-        var inview = new Waypoint.Inview({
-          element: $(this)[0],
-          enter: function(direction) {
-            if (direction == 'up') {
-              self.setTop($(this.element));
-              self.injectFixed($(this.element).next('.curtain'));
-            }
-          },
-          exit: function(direction) {
-            if (direction == 'down') {
-              self.setTop($(this.element));
-              self.injectFixed($(this.element).next('.curtain'));
-            }
-          }
-        })
-      });
-
-    }
-
-    self.setNext = function($el){
-      var $next = $el.next('.curtain');
-      $el.css('margin-bottom', $next.height());
-      setTimeout(function(){$next.addClass('set'); }, 10);
-    };
-
-    self.unsetNext = function($el){
-      var $next = $el.next('.curtain');
-      $el.css('margin-bottom', 0);
-      $next.removeClass('set');
-    };
-
-    self.init();
-  }
-
-  $(function() {
-    var $curtains = $(".curtain");
-
-    if($curtains.length) {
-      Curtain($curtains);
-    }
-  });
-
-})(jQuery);
+// (function($) {
+//
+//   function Curtain($curtains) {
+//     var self = this,
+//         $el = $curtains,
+//         waypoints = [],
+//         Waypoint = window.Waypoint;
+//
+//     self.init = function() {
+//       self.setWaypoints();
+//     }
+//
+//     self.setTop = function($el){
+//       $('.curtain').removeClass('top');
+//       $el.addClass('top');
+//     }
+//
+//     self.injectFixed = function($el){
+//       if ($el.length) {
+//         $('#fixed').empty();
+//         $el.children().first().clone().appendTo('#fixed');
+//       }
+//     }
+//
+//     self.setWaypoints = function(){
+//       $('.curtain').each(function(){
+//         var inview = new Waypoint.Inview({
+//           element: $(this)[0],
+//           enter: function(direction) {
+//             if (direction == 'up') {
+//               self.setTop($(this.element));
+//               self.injectFixed($(this.element).next('.curtain'));
+//             }
+//           },
+//           exit: function(direction) {
+//             if (direction == 'down') {
+//               self.setTop($(this.element));
+//               self.injectFixed($(this.element).next('.curtain'));
+//             }
+//           }
+//         })
+//       });
+//
+//     }
+//
+//     self.setNext = function($el){
+//       var $next = $el.next('.curtain');
+//       $el.css('margin-bottom', $next.height());
+//       setTimeout(function(){$next.addClass('set'); }, 10);
+//     };
+//
+//     self.unsetNext = function($el){
+//       var $next = $el.next('.curtain');
+//       $el.css('margin-bottom', 0);
+//       $next.removeClass('set');
+//     };
+//
+//     self.init();
+//   }
+//
+//   $(function() {
+//     var $curtains = $(".curtain");
+//
+//     if($curtains.length) {
+//       Curtain($curtains);
+//     }
+//   });
+//
+// })(jQuery);
 
 $('section ol').append('<div class="slideshow-nav">');
 
